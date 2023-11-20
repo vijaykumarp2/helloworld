e307e675a8220c2c0397afdcf7510f53f659b4b0
P <2134563@cognizant.com>
2023-11-20 13:14:38 +0530

    first commit

---
+++ src/main/java/com/example/githelloworld/GithelloworldApplication.java
@@ -0,0 +1,13 @@
+package com.example.githelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithelloworldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/githelloworld/GithelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
